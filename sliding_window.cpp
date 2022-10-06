a1b1145174dc71915844e5f8b560c675a49c353c
FishKick <106138736+FishKick@users.noreply.github.com>
2022-10-06 18:37:05 +0700

    Update sliding_window.cpp

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //inp:
 //9 4
 //1 4 2 10 2 3 1 0 20
-
+// 1 bài toán nũa là bắt in ra dãy con có tổng lớn nhât thì mình phải lấy đc vị trí đầu tiên của dãy con thỏa mãn đề bài || out << 3 1 0 20
 //out << 24
 
 
@@ -29,5 +29,37 @@ int main(){
 		}
 		cout << res << endl;
 	}
+	return 0;
+}
+
+int main(){
+	int t; cin >> t;
+	while (t--){
+		int n, k; cin >> n >> k;
+		int a[n];
+		
+		for(int& x : a) cin >> x;
+		int sum = 0;
+		for (int i = 0; i < k; i++){
+			sum+=a[i];
+		}
+		int res=sum;
+		int idx = 0;
+		for (int i = k; i < n; i++){
+			sum = sum - a[i-k] + a[i];
+			if (sum > res){
+				res = sum;
+				idx = i - k + 1;
+			}
+		}
+		
+		for (int i = 0; i < k;i++){
+			cout << a[idx + i] << " ";
+		}
+		cout << endl;
+	}
+	
+	
+
 	return 0;
 }
